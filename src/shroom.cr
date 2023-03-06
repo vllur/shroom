32e8ac14c69b2205775fbfb2a9c01498cfda9052
vllur <patryk.dejewski@disroot.org>
2023-03-06 14:35:28 +0100

    Basic functionality

--- src/shroom.cr
+++ src/shroom.cr
@@ -1,7 +1,33 @@
 require "totem"
+require "discordcr"
 
-config = Totem.from_file "_config.yml"
+module Shroom
+  struct Config
+    include JSON::Serializable
 
-p! config.get("token")
-p! config.get("channel")
-p! config.get("prefixes")
+    property token : String
+    property channel : UInt64
+    property prefixes : Array(String)
+    property commands : Hash(String, String)
+  end
+
+  config = Totem.from_file("_config.yml").mapping(Config)
+  client = Discord::Client.new(token: "Bot #{config.token}")
+
+  client.on_ready do
+    client.create_message(config.channel, "Hello")
+  end
+
+  client.on_message_create do |message|
+    # Act only on a message where first word is a prefix
+    next if message.content.empty? || !config.prefixes.join(" ").match(Regex.new(message.content.split(" ").first))
+
+    # Second word is the command
+    case message.content.split(" ")[1]
+    when config.commands["help"]
+      client.create_message(message.channel_id, "TODO")
+    end
+  end
+
+  client.run
+end
