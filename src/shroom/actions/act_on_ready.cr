b4461b1373b20f774628aa073718e54c884916f4
vllur <patryk.dejewski@disroot.org>
2023-03-07 13:58:19 +0100

    Customizable hello message

--- src/shroom/actions/act_on_ready.cr
+++ src/shroom/actions/act_on_ready.cr
@@ -1,7 +1,7 @@
 module Shroom
   def self.act_on_ready(client : Discord::Client, config : Config)
     client.on_ready do
-      client.create_message(config.channel, "Hello")
+      client.create_message(config.channel, config.hello) if config.enable_hello?
     end
   end
 end
--- src/shroom/config.cr
+++ src/shroom/config.cr
@@ -6,9 +6,11 @@ module Shroom
     getter channel : UInt64
     getter prefixes : Array(String)
     getter commands : Hash(String, String)
+    getter? enable_hello : Bool
+    getter hello : String
     getter help : String
 
-    def initialize(@token : String, @channel : UInt64, @prefixes : Array(String), @commands : Hash(String, String), @help : String)
+    def initialize(@token : String, @channel : UInt64, @prefixes : Array(String), @commands : Hash(String, String), @enable_hello : Bool, @hello : String, @help : String)
     end
 
     def self.load(filename)
